3ba87acc3c3e1ecbf5030410990171fd82c7798d
raimovasad <raimovasadbek18@gmail.com>
2021-08-26 18:14:56 +0500

    aside fixed !

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
-const MONGODB_URI = 'mongodb+srv://asad:b7q3JjGQzDpIfTfu@cluster0.l1arz.mongodb.net/CRM'
+const MONGODB_URI =
+  "mongodb+srv://asad:b7q3JjGQzDpIfTfu@cluster0.l1arz.mongodb.net/CRM";
 const session = require("express-session");
 const MongoDbStore = require("connect-mongodb-session")(session);
 const exhbs = require("express-handlebars");
 const app = express();
 const path = require("path");
 const isAdmin = require("./middleware/isAdmin");
-const Admin = require('./models/Admin')
-const Worker = require('./models/workers')
+const Admin = require("./models/Admin");
+const Worker = require("./models/workers");
 //  routes
 const workerRouter = require("./routes/worker/workers");
 const projectsRouter = require("./routes/projects/projects");
 const AuthRouter = require("./routes/auth/auth");
 const indexRouter = require("./routes/index");
-const lessonRouter = require("./routes/library/lessons"); 
+const lessonRouter = require("./routes/library/lessons");
 const libraryRouter = require("./routes/library/library");
 const traineesRouter = require("./routes/trainees/trainees-projects");
-const lidsRouter = require("./routes/lids/lids");   
+const lidsRouter = require("./routes/lids/lids");
 const salaryRouter = require("./routes/salary/salary");
-const priceRouter = require('./routes/price/price')
+const priceRouter = require("./routes/price/price");
 const store = new MongoDbStore({
   uri: MONGODB_URI,
   collection: "session",
 });
 
-const hbs = exhbs.create({ 
+const hbs = exhbs.create({
   defaultLayout: "admin",
   extname: "hbs",
   partialsDir: path.join(__dirname, "views/partials"),
@@ -34,13 +35,11 @@ const hbs = exhbs.create({
     allowProtoPropertiesByDefault: true,
     allowProtoMethodsByDefault: true,
   },
-  helpers:require('./helpers/Admin-manager')
-    
-  
+  helpers: require("./helpers/Admin-manager"),
 });
 
 // hbs.registerHelper("getTime", function(){
-     
+
 //   var myDate = new Date();
 //   var hour = myDate.getHours();
 //   var minute = myDate.getMinutes();
@@ -73,53 +72,50 @@ app.use(
     store,
   })
 );
-app.use(async(req,res,next)=>{
-  req.admin = req.session.admin 
-  if(req.admin){
-    res.locals.isadmin = req.session.isAdmin
-    res.locals.user = await Admin.findById(req.session.admin._id)
+app.use(async (req, res, next) => {
+  req.admin = req.session.admin;
+  if (req.admin) {
+    res.locals.isadmin = req.session.isAdmin;
+    res.locals.user = await Admin.findById(req.session.admin._id);
     res.locals.user = {
       fullname: res.locals.user.name,
-      avatar: res.locals.user.avatar
-    }
+      avatar: res.locals.user.avatar,
+    };
   }
-  req.worker = req.session.worker 
-   if(req.worker){
-     req.worker = req.session.worker
-     res.locals.isworker = req.session.isWorker
-    res.locals.user = await Worker.findById(req.worker._id.toString() )
+  req.worker = req.session.worker;
+  if (req.worker) {
+    req.worker = req.session.worker;
+    res.locals.isworker = req.session.isWorker;
+    res.locals.user = await Worker.findById(req.worker._id.toString());
+  }
+  req.student = req.session.student;
+  if (req.student) {
+    res.locals.isstudent = req.session.isStudent;
+    req.student = req.session.student;
+    res.locals.user = await Worker.findById(req.student._id.toString());
+  }
+  req.manager = req.session.manager;
+  if (req.manager) {
+    res.locals.ismanager = req.session.isManager;
+    req.manager = req.session.manager;
+    res.locals.user = await Worker.findById(req.manager._id.toString());
+  }
+  res.locals.isadmin = req.session.isAdmin;
 
-  }
-  req.student = req.session.student
-  if(req.student){
-    res.locals.isstudent = req.session.isStudent
-    req.student = req.session.student
-    res.locals.user = await Worker.findById(req.student._id.toString() )
-
-  }
-  req.manager = req.session.manager
-  if(req.manager){
-    res.locals.ismanager = req.session.isManager
-    req.manager = req.session.manager
-    res.locals.user = await Worker.findById(req.manager._id.toString() )
-  }
-  res.locals.isadmin = req.session.isAdmin
-
-  next()
-})
+  next();
+});
 
 app.use(flash());
 app.use("/auth", AuthRouter);
 app.use("/", isAdmin, indexRouter);
 app.use("/workers", isAdmin, workerRouter);
 app.use("/lesson", isAdmin, lessonRouter);
-app.use("/projects", isAdmin, projectsRouter); 
-app.use("/library", isAdmin,libraryRouter);
-app.use("/trainees", isAdmin,traineesRouter);
-app.use("/lids", isAdmin,lidsRouter);
+app.use("/projects", isAdmin, projectsRouter);
+app.use("/library", isAdmin, libraryRouter);
+app.use("/trainees", isAdmin, traineesRouter);
+app.use("/lids", isAdmin, lidsRouter);
 app.use("/salary", isAdmin, salaryRouter);
-app.use("/price", isAdmin, priceRouter); 
-
+app.use("/price", isAdmin, priceRouter);
 
 const PORT = process.env.PORT || 3000;
 
@@ -135,9 +131,4 @@ async function start() {
   });
 }
 
-
-
-
 start();
-
-
