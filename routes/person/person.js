7aa514cd9e866392c5d0b55c475d36ac702eb418
raimovasad <88774509+raimovasad@users.noreply.github.com>
2021-08-28 11:10:50 +0500

    add worker style edit and trainee project edit

--- routes/person/person.js
+++ routes/person/person.js
@@ -6,20 +6,29 @@ const rootUser = require('../../middleware/rootUser')
 const adminManWor =require('../../middleware/adminManWor')
 const adminMan = require('../../middleware/adminManager')
 const Admin = require('../../models/Admin')
+const Salary = require('../../models/Salary')
 
 
 router.get("/:id", async (req, res) => {
     const admin = await Admin.findOne()
-    const ifAdmin = Admin._id === req.params.id
-    
+    const ifAdmin = admin._id === req.params.id
+    const user = res.locals.user
+
+    let salary = await Salary.find()
+   salary =  salary.filter(c=> c.userId.toString() === user._id.toString())
+   salary = salary[0]
         const worker = await Workers.findById(req.params.id);
         res.render('person/person',{
             title: 'Personal page',
             ifAdmin,
+            salary,
             worker
+
         })
     
 });
+
+
 
 
 module.exports = router;
